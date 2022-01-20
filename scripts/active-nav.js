9735ab48171aba2a7770d9ffd2ab7d2c022d2754
Scott Jones <scottjones@Scotts-MacBook-Pro.local>
2022-01-19 23:02:08 -0800

    Improve active nav highlight functionality
    Home nav bar link active on initial page load

--- scripts/active-nav.js
+++ scripts/active-nav.js
@@ -1,6 +1,9 @@
 currentNav = document.getElementsByClassName('nav-text');
-	for(i = 0; i < currentNav.length; i++) { 
-		if(document.location.href.indexOf(currentNav[i].href)>=0) {
+homeNav = document.getElementById('home');
+
+for(i = 0; i < currentNav.length; i++) { 
+	if(document.location.href.indexOf(currentNav[i].href) >= 0) {
+		homeNav.classList.remove("active");
 		currentNav[i].classList.add("active");
 	}
 }
